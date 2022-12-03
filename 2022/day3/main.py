7888f5d6822e57f578436515db72cc404c745fb7
David Singleton <davidsingleton@gmail.com>
2022-12-03 00:02:29 -0800

    Day 3

---
+++ 2022/day3/main.py
@@ -0,0 +1,60 @@
+def char_to_score(ch):
+    t = ord(ch)
+    if t >= ord('a') and t <= ord('z'):
+        return t - ord('a') + 1
+    if t >= ord('A') and t <= ord('Z'):
+        return t - ord('A') + 27
+
+def first():
+    inp = open('input.txt', 'r')
+    lines = inp.readlines()
+
+    score = 0
+    for line in lines:
+        m = line.strip()
+        half = int(len(m) / 2)
+        print(half)
+        in_first= {}
+        left = m[0:half]
+        right = m[half:]
+        print(m)
+        print(len(left)," ",len(right))
+        print()
+        for char in left:
+            in_first[char] = True
+        for char in right:
+            if char in in_first:
+                score = score + char_to_score(char)
+                break
+    print(score)
+
+def second():
+    inp = open('input.txt', 'r')
+    lines = inp.readlines()
+
+    score = 0
+
+    foo = 0
+    common = {}
+    for line in lines:
+        m = line.strip()
+        for char in set(m):
+            if not char in common:
+                common[char] = 1
+            else:
+                common[char] = common[char] + 1
+        foo = foo + 1
+
+        if foo == 3:
+            for k in common:
+                print(k, common[k])
+                if common[k] == 3:
+                    score = score + char_to_score(k)
+                    print(k)
+                    break
+            foo = 0
+            common = {}
+    print(score)
+
+if __name__ == '__main__':
+    second()
