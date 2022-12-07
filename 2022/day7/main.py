6b63fad7ba0b04fb7f87f94e3a8d6e46e45f09bc
David Singleton <davidsingleton@gmail.com>
2022-12-06 22:06:52 -0800

    delete bad part now I have debugged it.

--- 2022/day7/main.py
+++ 2022/day7/main.py
@@ -1,7 +1,7 @@
 from utils import *
 input = [i.strip() for i in open("input.txt","r").readlines()]
 
-def p1():
+def part1():
     pwd = ""
     files = defaultdict(int)
     for line in input:
@@ -31,61 +31,9 @@ def p1():
     
     score = 0
     for dirs, size in acc.items():
+        print(dirs)
         if size <= 100000:
-            print(dirs)
             score += size
-    print(score)
-
-
-
-def part1():
-    pwd = '/'
-    phase = ""
-    files = defaultdict(int)
-    for line in input:
-        match line[0]:
-            case '$':
-                cmds = line.split(" ")
-                match cmds[1]:
-                    case "cd":
-                        match cmds[2]:
-                            case "..":
-                                dirs = pwd.split("/")
-                                dirs.pop()
-                                pwd = "/".join(dirs)
-                            case "/":
-                                pwd = "/"
-                            case _:
-                                next_dir = cmds[2]
-                                if pwd != "/":
-                                    pwd = pwd + "/" + next_dir
-                                else:
-                                    pwd = next_dir
-                    case "ls":
-                        phase = "ls"
-            case _:
-                if phase == "ls":
-                    toks = line.split(" ")
-                    if toks[0].isdigit():
-                        files[pwd + "/" + toks[1]] = int(toks[0])
-                else:
-                    assert(False)
-    print(files)
-    acc = defaultdict(int)
-    
-    for k,v in files.items():
-        k = k[1:]
-        print("aaa ", k)
-        for dir in k.split("/")[:-1]:
-            print("aa ", dir)
-            acc[dir] += v
-    score = 0
-    for dir, size in acc.items():
-        print(dir," ",size)
-        if size <= 100000:
-            print("**")
-            score += size
-
     print(score)
 
 def part2():
@@ -123,5 +71,5 @@ def part2():
     print(big_enough)
 
 if __name__ == '__main__':
-    #p1()
-    part2()
+    part1()
+    #part2()
