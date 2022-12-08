ebf072d28c58002809a6fb46bb7c5382b989a9e3
David Singleton <davidsingleton@gmail.com>
2022-12-07 23:13:29 -0800

    Day 8

---
+++ 2022/day8/main.py
@@ -0,0 +1,120 @@
+from utils import *
+from functools import reduce
+import operator
+input = [i.strip() for i in open("input.txt","r").readlines()]
+
+def part1():
+    score=set()
+    g = grid_from_strs(input).g()
+    # top
+    for col in range(len(g[0])):
+        cur = -1
+        h = -1
+        while cur < len(g[0]) - 1:
+            if int(g[cur+1][col]) > h:
+                h = int(g[cur+1][col])
+                score.add((cur+1, col))
+            cur += 1
+    # btm
+    for col in range(len(g[0])):
+        cur = len(g[0])
+        h = -1
+        while cur >= 0:
+            if int(g[cur-1][col]) > h:
+                h = int(g[cur-1][col])
+                score.add((cur-1, col))
+            cur -= 1
+    # left
+    for row in range(len(g[0])):
+        cur = -1
+        h = -1
+        while cur < len(g[0]) - 1:
+            if int(g[row][cur+1]) > h:
+                h = int(g[row][cur+1])
+                score.add((row, cur+1))
+            cur += 1
+    # right
+    for row in range(len(g[0])):
+        cur = len(g[0])
+        h = -1
+        while cur >= 0:
+            if int(g[row][cur-1]) > h:
+                print("adding ", (row, cur-1), g[row][cur-1], h)
+                h = int(g[row][cur-1])
+                score.add((row, cur-1))
+            cur -= 1
+    print(score)
+
+
+def cs(g, x, y):
+    s = len(g.g()[0])
+    cs = []
+    g.set_cursor(x,y)
+    max = int(g.get())
+    # up
+    h = -1
+    c = 0
+    while g._cursor[1] > 0:
+        g.move(0,-1)
+        if int(g.get()) >= max:
+            c+=1
+            break
+        if int(g.get()) >= h:
+            c+=1
+    cs.append(c)
+    # down
+    h = -1
+    c = 0
+    g.set_cursor(x,y)
+    while g._cursor[1] < s-1:
+        g.move(0,1)
+        if int(g.get()) >= max:
+            c+=1
+            break
+        if int(g.get()) >= h:
+            c+=1
+    cs.append(c)
+    # left
+    h = -1
+    c = 0
+    g.set_cursor(x,y)
+    while g._cursor[0] > 0:
+        g.move(-1,0)
+        if int(g.get()) >= max:
+            c+=1
+            break
+        if int(g.get()) >= h:
+            c+=1
+    cs.append(c)
+    # right
+    h = -1
+    c = 0
+    g.set_cursor(x,y)
+    while g._cursor[0] < s-1:
+        g.move(1,0)
+        if int(g.get()) >= max:
+            c+=1
+            break
+        if int(g.get()) >= h:
+            c+=1
+    cs.append(c)
+    return reduce(operator.mul, cs, 1)
+
+def part2():
+    g = grid_from_strs(input)
+    print(g)
+    max_scene = 0
+    mt = None
+    for row in range(1,len(g.g()[0])-1):
+        for col in range(1,len(g.g()[0])-1):
+            x = cs(g, row, col)
+            if x > max_scene:
+                print(max_scene, mt)
+                max_scene = x
+                mt = (row,col)
+    print(max_scene)
+
+
+if __name__ == '__main__':
+    #part1()
+    part2()
