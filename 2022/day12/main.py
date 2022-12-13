706164a97b56aebf50550481ad5ac86b5bea9d31
David Singleton <davidsingleton@gmail.com>
2022-12-12 20:48:55 -0800

    Much better solution

--- 2022/day12/main.py
+++ 2022/day12/main.py
@@ -55,6 +55,55 @@ def solve():
                 min = i[0]
     print(min)
 
+def delt(here, there):
+    if there == 'S':
+        return ord("a") - ord(here)
+    if there == 'E':
+        return ord("z") - ord(here)
+    if here == 'S':
+        return ord(there) - ord("a")
+    if here == 'E':
+        return ord(there) - ord("z")
+
+    return ord(there) - ord(here)
+
+def can_walk(r,c,ch):
+    ret = []
+    for _,i in DIR.items():
+        if r + i[1] < len(input) and r + i[1] >= 0 and c + i[0] < len(input[0]) and c + i[0] >= 0:
+            if delt(ch, input[r+i[1]][c + i[0]]) <= 1:
+                ret.append(str(r+i[1])+","+str(c+i[0]))
+    return ret
+
+def solve2():
+    graph = {}
+    ayes = []
+    start, end = None, None
+    for r, row in enumerate(input):
+        for c, ch in enumerate(row):
+            key = str(r)+","+str(c)
+            graph[key] = can_walk(r,c,ch)
+            if ch == 'S':
+                start = str(r)+","+str(c)
+                ayes.append(str(r)+","+str(c))
+            if ch == 'E':
+                end = str(r)+","+str(c)
+            if ch == 'a':
+                ayes.append(str(r)+","+str(c))
+
+    print(len(find_shortest_path(graph, start, end)) - 1)
+
+    min_path = 9999999
+    for a in ayes:
+        path = find_shortest_path(graph, a, end)
+        if path:
+            t = len(path) - 1
+            if t < min_path:
+                min_path = t
+    print(min_path)
+            
+    pass
 
 if __name__ == '__main__':
     solve()
+    solve2()
