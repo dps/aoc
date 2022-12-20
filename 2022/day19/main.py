696edc4729f1b46620881a3f356c5f0a72169ae1
David Singleton <davidsingleton@gmail.com>
2022-12-20 09:21:03 +0000

    day19

--- 2022/day19/main.py
+++ 2022/day19/main.py
@@ -1,10 +1,9 @@
 from utils import *
-import random
-from math import ceil
 
-input = [i.strip() for i in open("simple.txt","r").readlines()]
+input = [i.strip() for i in open("input.txt","r").readlines()]
 
 blueprints = {}
+global_best = 0
 
 def can_build(num, resources):
     costs = blueprints[num]["costs"]
@@ -24,7 +23,6 @@ def can_build(num, resources):
 
             build.append((b,c,i))
 
-    #print("lcb", build)
     return build
 
 @cache
@@ -36,25 +34,33 @@ def max_cost(num, resource_num):
     print("max_cost", num, resource_num, m)
     return m
 
+@cache
+def max_geodes_from_new_bots_in_remaining(mins):
+    if mins <= 1:
+        return 0
+    elif mins == 2:
+        return 1
+    else:
+        return triangle(mins - 1)
+
 #resource order: ore, clay, obsidian, geodes
 @cache
 def maximize(num, robots, resources, mins_remaining):
-    #print("maximize", num, robots, resources, mins_remaining)
+    global global_best
     if mins_remaining == 0:
-        #print("0 mins", resources[3])
+        if resources[3] > global_best:
+            global_best = resources[3]
         return resources[3]
+
+    # if (resources[3] + robots[3] * mins_remaining + max_geodes_from_new_bots_in_remaining(mins_remaining)) < global_best:
+    #     return 0
     could_spend = can_build(num, resources)
 
     to_prune = []
-    might_build_this_round = set()
     if len(could_spend) > 1:
         for option in could_spend:
-            if robots[option[2]] >= max_cost(num, option[2]):
+            if robots[option[2]] * mins_remaining + resources[option[2]] > mins_remaining * max_cost(num, option[2]):
                 to_prune.append(option)
-            elif robots[option[2]] * mins_remaining + resources[option[2]] > mins_remaining * max_cost(num, option[2]):
-                to_prune.append(option)
-            else:
-                might_build_this_round.add(option[2])
 
     for p in to_prune:
         could_spend.remove(p)
@@ -69,33 +75,21 @@ def maximize(num, robots, resources, mins_remaining):
     # although it consumes the necessary resources available when construction begins.
     best = 0
     for bom in could_spend:
-        if bom[2] != None:
-            next_resources = (resources[0] + robots[0] - bom[1][0],
-                            resources[1] + robots[1] - bom[1][1],
-                            resources[2] + robots[2] - bom[1][2],
-                            resources[3] + robots[3])
-            next_robots = (robots[0] + bom[0][0],
-                        robots[1] + bom[0][1],
-                        robots[2] + bom[0][2],
-                        robots[3] + bom[0][3],
-                        )
-            best = max(best, maximize(num, next_robots, next_resources, mins_remaining - 1))
-        else:
-            next_resources = (resources[0] + robots[0] - bom[1][0],
-                            resources[1] + robots[1] - bom[1][1],
-                            resources[2] + robots[2] - bom[1][2],
-                            resources[3] + robots[3])
-            next_robots = (robots[0] + bom[0][0],
-                        robots[1] + bom[0][1],
-                        robots[2] + bom[0][2],
-                        robots[3] + bom[0][3],
-                        )
-            best = max(best, maximize(num, next_robots, next_resources, mins_remaining - 1))
+        next_resources = (resources[0] + robots[0] - bom[1][0],
+                        resources[1] + robots[1] - bom[1][1],
+                        resources[2] + robots[2] - bom[1][2],
+                        resources[3] + robots[3])
+        next_robots = (robots[0] + bom[0][0],
+                    robots[1] + bom[0][1],
+                    robots[2] + bom[0][2],
+                    robots[3] + bom[0][3],
+                    )
+        best = max(best, maximize(num, next_robots, next_resources, mins_remaining - 1))
 
     return best
     
 
-def part1():
+def solve(part=1):
     for i, line in enumerate(input):
         blueprint = {}
         #Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 8 clay. Each geode robot costs 2 ore and 15 obsidian.
@@ -120,50 +114,21 @@ def part1():
     print(blueprints)
 
 
-    tot = 0
+    tot = 0 if part == 1 else 1
     for k,blueprint in blueprints.items():
-        m = solve(k, (1,0,0,0), (0,0,0,0), 24)
+        m = maximize(k, (1,0,0,0), (0,0,0,0), 24 if part==1 else 32)
         print("***>", k, m)
-        tot += k * m
-
-    print(tot)
-    return tot
-
-def part2():
-    for i, line in enumerate(input):
-        blueprint = {}
-        #Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 8 clay. Each geode robot costs 2 ore and 15 obsidian.
-
-        num = ints(line.split(":")[0])[0]
-        robot_descs = line.split(":")[1].split(".")
-        for robot in robot_descs:
-            print(robot)
-            g = re.match(r"Each (\w+) robot costs ([^.]*)", robot.strip())
-            if not g:
-                break
-            name = g.groups()[0]
-            material_string = g.groups()[1]
-            materials = {x[1]:x[0] for x in [re.match(r"(\d+) (\w+)", m.strip()).groups() for m in material_string.split("and")]}
-            dd = defaultdict(int)
-            for k,v in materials.items():
-                dd[k] = int(v)
-            blueprint[name] = (dd["ore"], dd["clay"], dd["obsidian"])
-        blueprint["n"] = int(num)
-        blueprint["costs"] = (blueprint["ore"], blueprint["clay"], blueprint["obsidian"], blueprint["geode"])
-        blueprints[int(num)] = blueprint
-    print(blueprints)
-
-
-    tot = 1
-    for k,blueprint in blueprints.items():
-        m = maximize(k, (1,0,0,0), (0,0,0,0), 32) #24,32
-        print("***>", k, m)
-        tot *= m
-        if (k == 3):
+        if part == 1:
+            tot += k * m
+        else:
+            tot *= m
+        if (part == 2 and k == 3):
             break
 
     print(tot)
     return tot
 
+
 if __name__ == '__main__':
-    part1()
+    assert(solve(1) == 1404)
+    assert(solve(2) == 5880)
