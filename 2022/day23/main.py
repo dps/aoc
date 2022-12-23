28319de6d8957a94ac0abbde5cf9c0bee1a57e18
David Singleton <davidsingleton@gmail.com>
2022-12-23 09:09:59 +0000

    day23

---
+++ 2022/day23/main.py
@@ -0,0 +1,56 @@
+from utils import *
+input = [i.strip() for i in open("input.txt","r").readlines()]
+
+
+MDIR8 = [p[0] + 1j*p[1] for _,p in DIR8.items()]
+
+def solve(max_rounds):
+    world = set()
+    moves = deque([ ((-1j,1-1j,-1-1j), -1j, "N"),
+                    ((1j,1+1j,-1+1j), 1j, "S"),
+                    ((-1,-1+1j,-1-1j), -1, "W"),
+                    ((1,1+1j,1-1j), 1, "E")])
+
+    for y, row in enumerate(input):
+        for x, ch in enumerate(row):
+            if ch == '#':
+                world.add(x+y*1j)
+
+    for round in range(max_rounds):
+        proposed = defaultdict(int)
+        elf_mv = {}
+        for elf in world:
+            if not any([(elf + e) in world for e in MDIR8]):
+                continue
+
+            for mv in moves:
+                if (elf + mv[0][0] not in world and
+                    elf + mv[0][1] not in world and
+                    elf + mv[0][2] not in world):
+                    proposed[elf + mv[1]] += 1
+                    elf_mv[elf] = elf + mv[1]
+                    break
+        new_world = deepcopy(world)
+        for elf in world:
+            if elf in elf_mv and proposed[elf_mv[elf]] == 1:
+                new_world.remove(elf)
+                new_world.add(elf_mv[elf])
+        moves.rotate(-1)
+        if (new_world == world):
+            print("pt2 DONE at round", round+1)
+            return
+        world = new_world
+
+        mix,mxx = int(min([x.real for x in world])), int(max([x.real for x in world]))
+        miy,myy = int(min([x.imag for x in world])), int(max([x.imag for x in world]))
+
+    space = 0
+    for y in range(miy,myy+1):
+        for x in range(mix,mxx+1):
+            if (x+y*1j) not in world:
+                space += 1
+    print("pt1 spaces after max_rounds", space) 
+
+if __name__ == '__main__':
+    solve(10)
+    solve(1000)
