a4e953d1f7420f1c40f2bce503510fd977d624e2
David Singleton <davidsingleton@gmail.com>
2022-12-24 16:27:46 +0000

    Speedup

--- 2022/day24/main.py
+++ 2022/day24/main.py
@@ -1,6 +1,5 @@
 from utils import *
 input = [i.strip() for i in open("input.txt","r").readlines()]
-
 blizzards = deque([])
 max_x, max_y = 0,0
 
@@ -21,27 +20,33 @@ MV = {'<': -1, '>': 1, '^': -1j, 'v': 1j, '$': 0}
 @cache
 def blizzards_at(mins):
     if mins == 0:
-        return blizzards
-    prev = blizzards_at(mins - 1)
+        b_set = set()
+        for b in blizzards:
+            b_set.add(b[1])
+        return blizzards, b_set
+    prev, _ = blizzards_at(mins - 1)
     this_min = deque([])
+    this_min_set = set()
     for blizzard in prev:
         mv = MV[blizzard[0]]
-        this_min.append((blizzard[0], wrap(blizzard[1] + mv)))
-    return this_min
+        next_pos = wrap(blizzard[1] + mv)
+        this_min.append((blizzard[0], next_pos))
+        this_min_set.add(next_pos)
+    return this_min, this_min_set
 
 def bfs(pos, goal):
     q = deque([(pos, 0)])
     visited = set()
     while len(q) > 0:
         p = q.popleft()
-        blizz = blizzards_at(p[1] + 1)
+        _, blizz_set = blizzards_at(p[1] + 1)
         for move in MV.values():
             next = p[0] + move
             if next == goal:
                 return p[1] + 1
             if next != pos and (next.real < 0 or next.real > max_x or next.imag < 0 or next.imag > max_y):
                 continue
-            if not any([b[1] == next for b in blizz]):
+            if not next in blizz_set:
                 if (next, p[1] + 1) not in visited:
                     q.append((next, p[1] + 1))
                     visited.add((next, p[1] + 1))
@@ -63,12 +68,12 @@ def solve():
     initial_crossing = bfs(pos, goal)
     print("Part 1 answer:", initial_crossing, "mins")
 
-    blizzards = blizzards_at(initial_crossing)
+    blizzards, _ = blizzards_at(initial_crossing)
     blizzards_at.cache_clear()
 
     return_journey = bfs(goal, pos)
 
-    blizzards = blizzards_at(return_journey)
+    blizzards, _ = blizzards_at(return_journey)
     blizzards_at.cache_clear()
 
     back_again = bfs(pos, goal)
@@ -76,6 +81,6 @@ def solve():
     print("Part 2 answer:", initial_crossing + return_journey + back_again, "mins")
 
 if __name__ == '__main__':
-    # Runtime: DFS 54.58s -> 40.86s [added manhattan stop] -> 18.09s [BFS]
+    # Runtime: DFS 54.58s -> 40.86s [added manhattan stop] -> 18.09s [BFS] -> 1.06s [set for blizzard test]
     # input is 122x26
     solve()
