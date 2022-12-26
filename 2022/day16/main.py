6b6a3e2eace4b78f9b966ce64e0c613e87667888
David Singleton <davidsingleton@gmail.com>
2022-12-26 22:34:11 +0000

    remove unused import

--- 2022/day16/main.py
+++ 2022/day16/main.py
@@ -2,7 +2,6 @@ import math
 from utils import *
 from functools import cache
 from functools import cmp_to_key
-from itertools import permutations
 
 input = [i.strip() for i in open("input.txt","r").readlines()]
 
@@ -92,21 +91,3 @@ def solve():
     return (part1, part2)
 
 assert(solve() == (2320, 2967))
-
-
-# Progress:
-# Initial DFS implementation without frozenset:
-## time python main.py 
-## python main.py  95.10s user 1.09s system 99% cpu 1:36.24 total
-# With frozenset:
-## python main.py  55.31s user 1.35s system 99% cpu 56.710 total
-# With pypy:
-## 40.93s user 1.75s system 99% cpu 42.686 total
-# Remove non-flow valves (BFS dist table)
-## 39.15s user 0.39s system 99% cpu 39.555 total
-# int bitmask instead of frozenset
-## 34.73s user 0.48s system 99% cpu 35.222 total
-# floyd-warshall instead of BFS
-## 31.14s user 0.55s system 99% cpu 31.733 total
-# opened valves statemap !!!
-## 1.48s user 0.12s system 98% cpu 1.624 total
