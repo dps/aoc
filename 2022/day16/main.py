0be7750d5fb89ee3be96bc84ea3338e4f52310d5
David Singleton <davidsingleton@gmail.com>
2022-12-26 20:41:01 +0000

    Floyd-Warshall

--- 2022/day16/main.py
+++ 2022/day16/main.py
@@ -1,3 +1,4 @@
+import math
 from utils import *
 from functools import cache
 from functools import cmp_to_key
@@ -10,16 +11,17 @@ tvalves = 0
 dist = {}
 start = 999
 
-def floyd_warshall(graph):
+def floyd_warshall(graph, bidirectional=False):
     # Given a graph dict of format {vertex: [edges]}
     # returns the shortest path between every pair of nodes in the graph.
-    dist = {}
+    dist = defaultdict(lambda : math.inf)
     for node, edges in graph.items():
         for dest in edges:
             dist[(node, dest)] = 1 # use weight if weighted
-            dist[(dest, node)] = 1
-    for node in graph.keys():
-        dist[(node, node)] = 0
+            if bidirectional:
+                dist[(dest, node)] = 1
+    # for node in graph.keys():
+    #     dist[(node, node)] = 0
     for k in graph.keys():
         for i in graph.keys():
             for j in graph.keys():
@@ -57,7 +59,7 @@ def dfs(here, mins_remaining, bitmask, players_remaining):
     return best
 
 def solve():
-    global tvalves, start
+    global tvalves, start, dist, connect
 
     all_conn = {}
     for row in input:
@@ -73,35 +75,22 @@ def solve():
         flow[valve] = rate
         all_conn[valve] = tunnels
 
-    remap = {p[0]:i for i,p in enumerate(sorted([(k,v) for k,v in flow.items() if k == "AA" or v > 0],
+    remap = {p[0]:i for i,p in enumerate(sorted([(k,v) for k,v in flow.items()],
         key=cmp_to_key(lambda x,y: y[1]-x[1])))}
     start = remap["AA"]
 
-    q = deque([])
-    for valve, _ in ([("AA", 0)] + [(v,k) for v,k in flow.items() if k > 0]):
-        q.append((valve, 0))
-        visited = set()
-        while len(q) > 0:
-            p = q.popleft()
-            visited.add(p[0])
-            if flow[p[0]] > 0 and p[0] != valve:
-                dist[(remap[valve], remap[p[0]])] = p[1]
-                connect[remap[valve]].append(remap[p[0]])
-                if valve != "AA": # Never go back to the start
-                    dist[(remap[p[0]], remap[valve])] = p[1]
-                    connect[remap[p[0]]].append(remap[valve])
-            for c in all_conn[p[0]]:
-                if c not in visited:
-                    q.append((c, p[1] + 1))
+    dist_pairs = floyd_warshall(all_conn)
 
-    # dist = floyd_warshall({remap[v]: es for (v,es) in all_conn.items()})
-    # connect = {node:edges for (node, edges) in all_conn if flow[node] > 0 or node == "AA"}
+    for (n,o), d in dist_pairs.items():
+        if d != math.inf and flow[o] > 0:
+            connect[remap[n]].append(remap[o])
+            dist[(remap[n],remap[o])] = d
 
     tvalves = (1 << (len([(k,v) for k,v in flow.items() if k == "AA" or v > 0]) - 2)) - 1
 
     for k,v in remap.items():
         flow[v] = flow[k]
-
+        del(flow[k])
 
     assert(dfs(start, 30, 0, 0) == 2320)
     assert(dfs(start, 26, 0, 1) == 2967)
@@ -121,3 +110,5 @@ solve()
 ## 39.15s user 0.39s system 99% cpu 39.555 total
 # int bitmask instead of frozenset
 ## 34.73s user 0.48s system 99% cpu 35.222 total
+# floyd-warshall instead of BFS
+## 31.14s user 0.55s system 99% cpu 31.733 total
