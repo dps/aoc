7014ecb540a8ab5b3b8a0947fd73f01ef8ff696d
David Singleton <davidsingleton@gmail.com>
2022-12-29 01:41:42 +0000

    cleanup

--- 2022/day19/main.py
+++ 2022/day19/main.py
@@ -9,7 +9,7 @@ for i, line in enumerate(input):
     max_ore, max_clay, max_obsidian = max([c[0] for c in bots]), max([c[1] for c in bots]), max([c[2] for c in bots])
     recipes.append((bots, (max_ore, max_clay, max_obsidian, math.inf)))
 
-def max_geodes_from_new_bots_in_remaining(mins):
+def max_geodes_from_new_bots(mins):
     return triangle(mins - 1)
 
 global_best = 0
@@ -17,15 +17,11 @@ global_best = 0
 def dfs(n, robots, resources, mins_remaining):
     global global_best
     if mins_remaining == 1:  # This case => 45x speedup!
-        if global_best < resources[3] + robots[3]:
-            global_best = resources[3] + robots[3]
         return resources[3] + robots[3]
     if mins_remaining <= 0:
-        if global_best < resources[3]:
-            global_best = resources[3]
         return resources[3]
 
-    if (resources[3] + robots[3] * mins_remaining + max_geodes_from_new_bots_in_remaining(mins_remaining)) <= global_best:
+    if (resources[3] + robots[3] * mins_remaining + max_geodes_from_new_bots(mins_remaining)) <= global_best:
         return 0
     
     bots, maxes = recipes[n]
@@ -33,24 +29,25 @@ def dfs(n, robots, resources, mins_remaining):
     for b, bot in enumerate(bots):
         if (resources[b]//mins_remaining) + robots[b] >= maxes[b]:
             continue
-        wait_mins = max([(ceil((r-resources[i])/robots[i]) if robots[i] > 0 else math.inf) if r > 0 else 0 for i,r in enumerate(bot)]) + 1
-        if wait_mins != math.inf:
-            if wait_mins >= mins_remaining:
-                continue
-            new_robots = tuple(robots[i] + 1 if b==i else robots[i] for i in range(4))
+        wait_mins = 1 + max([(ceil((r-resources[i])/robots[i]) if robots[i] > 0 else math.inf)
+                             if r > 0 else 0 for i,r in enumerate(bot)])
+        if wait_mins < mins_remaining:
+            new_robots = tuple(robots[i] + (1 if b==i else 0) for i in range(4))
             new_resources = tuple(min(resources[i] + robots[i]*wait_mins - bot[i], maxes[i]*mins_remaining) for i in range(4))
             best = max(best, dfs(n, new_robots, new_resources, mins_remaining - wait_mins))
 
+    if global_best < best:
+        global_best = best
     return best
 
 ### Speedups
 # Rewrote to branch on next robot choice instead of mins_remaining (6x speedup)
-# Write a case for mins_remaining = 1 (45x speedup)
+# Wrote a case for mins_remaining = 1 (45x speedup)
 # If this branch can do no better than the global max already seen, return early (6x speedup)
 # (orig) if robots[b] > maxes[b], don't build excess production capacity (14x speedup)
-# 
-# if (resources[b]//mins_remaining) + robots[b] >= maxes[b] (30% speedup)
-# - if the resources we have in hand plus the robots we have allow us to buld any robot in any remaining min, don't build more of that robot type.
+# (slightly better) if (resources[b]//mins_remaining) + robots[b] >= maxes[b] (30% speedup)
+# - if the resources we have in hand plus the robots we have allow us to buld any robot in any remaining min,
+#   don't build more of that robot type.
 # min(resources[i] + robots[i]*wait_mins - bot[i], maxes[i]*mins_remaining)
 # - If we have resources we can never use, throw them away, reduces the search space. (minor speedup)
 
@@ -72,6 +69,6 @@ def part2():
     print(acc)
     return acc
 
-
+#~/pypy3.9/bin/python main.py  0.91s user 0.05s system 98% cpu 0.969 total
 assert(part1() == 1404)
 assert(part2() == 5880)
