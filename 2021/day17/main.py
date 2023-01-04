eaf4c27d5a920417810aa6240557c45b3e6867f0
David Singleton <davidsingleton@gmail.com>
2023-01-03 22:59:50 -0800

    2021 day17

--- 2021/day17/main.py
+++ 2021/day17/main.py
@@ -19,22 +19,24 @@ def simulate(vx,vy):
             return True, sim_max_y
 
 def part1():
-    mh = 0
-    for vy in range(1,-min_y):
-        for vx in range(1,max_x):
-            h = simulate(vx,vy)
-            if h[0] == True:
-                mh = max(h[1], mh)
-            else:
-                _, _, too_short, too_long, _ = h
-                if too_long:
-                    break 
-    print(mh)
+    print(((min_y+1)*min_y)//2)
+    # mh = 0
+    # for vy in range(1,-min_y):
+    #     for vx in range(1,max_x):
+    #         h = simulate(vx,vy)
+    #         if h[0] == True:
+    #             mh = max(h[1], mh)
+    #         else:
+    #             _, _, too_short, too_long, _ = h
+    #             if too_long:
+    #                 break 
+    # print(mh)
 
 def part2():
     t = 0
+    min_x_search = int((min_x * 2) ** 0.5 - 1)
     for vy in range(min_y,-min_y):
-        for vx in range(1,max_x+1):
+        for vx in range(min_x_search, max_x+1):
             h = simulate(vx,vy)
             if h[0] == True:
                 t+=1
