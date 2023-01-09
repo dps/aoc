69254bff8cc3b8c13dedb59eb648a2bc8e911ece
David Singleton <davidsingleton@gmail.com>
2023-01-08 22:07:12 -0800

    2021 day22

--- 2021/day22/main.py
+++ 2021/day22/main.py
@@ -1,7 +1,6 @@
 from utils import *
-from dataclasses import dataclass
 
-input = [i.strip() for i in open("simple.txt","r").readlines()]
+input = [i.strip() for i in open("input.txt","r").readlines()]
 
 
 def part1():
@@ -47,105 +46,26 @@ def box_intersection(a,b):
 def volume(range):
     return abs(range[5]+1-range[4])*abs(range[3]+1-range[2])*abs(range[1]+1-range[0])
 
-def cut_up_boxes(box1, box2, box_intersect):
-    # box1 and box2 are [min_x, max_x, min_y, max_y, min_z,max_z]
-    # representing the range of values the box spans in each dimension
-    # box intersection is the intersect also of the form [min_x, max_x, min_y, max_y, min_z,max_z]
-    # returns (box_intersect, box1_cuts, box2_cuts)
-
-    # now we'll cut up the first box
-    box1_cuts = []
-    if box_intersect[0] > box1[0]:
-        # there's a non-intersecting region on the left side of the first box in the x dimension
-        box1_cuts.append([box1[0], box_intersect[0]-1, box1[2], box1[3], box1[4], box1[5]])
-    if box_intersect[1] < box1[1]:
-        # there's a non-intersecting region on the right side of the first box in the x dimension
-        box1_cuts.append([box_intersect[1]+1, box1[1], box1[2], box1[3], box1[4], box1[5]])
-    if box_intersect[2] > box1[2]:
-        # there's a non-intersecting region on the bottom side of the first box in the y dimension
-        box1_cuts.append([box1[0], box1[1], box1[2], box_intersect[2]-1, box1[4], box1[5]])
-    if box_intersect[3] < box1[3]:
-        # there's a non-intersecting region on the top side of the first box in the y dimension
-        box1_cuts.append([box1[0], box1[1], box_intersect[3]+1, box1[3], box1[4], box1[5]])
-    if box_intersect[4] > box1[4]:
-        # there's a non-intersecting region on the back side of the first box in the z dimension
-        box1_cuts.append([box1[0], box1[1], box1[2], box1[3], box1[4], box_intersect[4]-1])
-    if box_intersect[5] < box1[5]:
-        # there's a non-intersecting region on the front side of the first box in the z dimension
-        box1_cuts.append([box1[0], box1[1], box1[2], box1[3], box_intersect[5]+1, box1[5]])
-    
-    # now we'll cut up the second box
-    box2_cuts = []
-    if box_intersect[0] > box2[0]:
-        # there's a non-intersecting region on the left side of the second box in the x dimension
-        box2_cuts.append([box2[0], box_intersect[0], box2[2], box2[3], box2[4], box2[5]])
-    if box_intersect[1] < box2[1]:
-        # there's a non-intersecting region on the right side of the second box in the x dimension
-        box2_cuts.append([box_intersect[1], box2[1], box2[2], box2[3], box2[4], box2[5]])
-    if box_intersect[2] > box2[2]:
-        # there's a non-intersecting region on the bottom side of the second box in the y dimension
-        box2_cuts.append([box2[0], box2[1], box2[2], box_intersect[2], box2[4], box2[5]])
-    if box_intersect[3] < box2[3]:
-        # there's a non-intersecting region on the top side of the second box in the y dimension
-        box2_cuts.append([box2[0], box2[1], box_intersect[3], box2[3], box2[4], box2[5]])
-    if box_intersect[4] > box2[4]:
-        # there's a non-intersecting region on the back side of the second box in the z dimension
-        box2_cuts.append([box2[0], box2[1], box2[2], box2[3], box2[4], box_intersect[4]])
-    if box_intersect[5] < box2[5]:
-        # there's a non-intersecting region on the front side of the second box in the z dimension
-        box2_cuts.append([box2[0], box2[1], box2[2], box2[3], box_intersect[5], box2[5]])
-
-    return (box_intersect, box1_cuts, box2_cuts)
-
-i = box_intersection([0,10,0,10,0,10], [5,6,5,6,5,6])
-i,a,b = cut_up_boxes([0,10,0,10,0,10], [5,6,5,6,5,6], i)
-print(a[0])
-print(a[1])
-print(a[2])
-print(volume(i), volume(a[0])+volume(a[1])+volume(a[2]), volume([0,10,0,10,0,10]))
+# I ended up cribbing this a bit from the solution megathread - I'd figured out this algorithm
+# but had some off by one type bug and couldn't get it quite right...
 def part2():
-    sequence = []
-    procedure = []
-    for i, line in enumerate(input):
+    region_counts = defaultdict(int)
+    for line in input:
         action = line.split(" ")[0]
-        range = ints(line)
-        procedure.append((action, range, i))
+        range = tuple(ints(line))
+
+        to_merge = defaultdict(int)
+        for existing_range, current_dir in region_counts.items():
+            isect = box_intersection(range, existing_range)
+            if isect:
+                to_merge[tuple(isect)] -= current_dir
         if action == "on":
-            for s in sequence:
-                isect = box_intersection(range, s[0])
-                if isect:
-                    s[1].append(isect) # append our intersection to s's offs so we don't double count
-            sequence.append([range, []])
-        elif action == "off":
-            for s in sequence:
-                isect = box_intersection(range, s[0])
-                if isect:
-                    s[1].append(isect)
-    print(sequence)
-    total = 0
-    for s in sequence:
-        stot = volume(s[0])
-        for i, off in enumerate(s[1]):
-            off_cuts = [off]
-            if i > 0:
-                for prev in s[1][:i]:
-                    to_remove = []
-                    to_add = []
-                    for part in off_cuts:
-                        isect = box_intersection(prev, part)
-                        if isect:
-                            _, more_off_cuts, _ = cut_up_boxes(part, prev, isect)
-                            to_remove.append(part)
-                            to_add.extend(more_off_cuts)
-                    for r in to_remove:
-                        off_cuts.remove(r)
-                    for a in to_add:
-                        off_cuts.append(a)
-                stot -= sum([volume(o) for o in off_cuts])
-            else:
-                stot -= volume(off)
-        total += stot
-    print(total)
+            to_merge[range] += 1
+        for range, count in to_merge.items():
+            region_counts[range] += count
+
+    print(sum(volume(range) * dir for range, dir in region_counts.items()))
+
 
 
 
