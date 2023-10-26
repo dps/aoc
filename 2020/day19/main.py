dd25f468294b3154fa2487472ec030061ec4ff3c
David Singleton <davidsingleton@gmail.com>
2023-10-25 21:15:23 -0700

    2020 day19

--- 2020/day19/main.py
+++ 2020/day19/main.py
@@ -3,7 +3,7 @@ import operator
 
 input = [i.strip() for i in open("input.txt","r").readlines()]
 
-def solve():
+def solve(part=1):
     i = bundles(input)
     rules, messages = next(i), next(i)
 
@@ -15,14 +15,23 @@ def solve():
         if r[0] == '"':
             return r[1]
         if "|" in r:
-            parts = r.split("|")
-            return "((" + construct(parts[0].strip()) + ")|(" + construct(parts[1].strip()) + "))"
+            parts = r.split(" | ")
+            return "((" + construct(parts[0]) + ")|(" + construct(parts[1]) + "))"
         cons = ""
         for tok in r.split(" "):
-            cons += construct(rs[tok.strip()])
+            if part == 2 and tok == "8":
+                cons += construct(rs[tok]) + "+"
+            elif part == 2 and tok == "11":
+                l = construct(rs["42"])
+                r = construct(rs["31"])
+                # 4 levels deep is all that was required to fully match my input.
+                cons += "((" + l + r + ")|(" + 2*l + 2*r + ")|(" + 3*l + 3*r + ")|(" + 4*l + 4*r + "))"
+            else:
+                cons += construct(rs[tok])
         return cons
     
     regex = construct(rs["0"])
-    aoc(sum([1 if re.fullmatch(regex, m) != None else 0 for m in messages]))
+    aoc(sum([1 for m in messages if re.fullmatch(regex, m) != None]))
 
-solve()
+solve(1)
+solve(2)
