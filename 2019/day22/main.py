f9db5d60dcdaa6d50fa3d181c4c339b93ad2f22e
David Singleton <davidsingleton@gmail.com>
2023-11-14 22:41:09 -0800

    2019 day22

--- 2019/day22/main.py
+++ 2019/day22/main.py
@@ -128,7 +128,7 @@ def increment(top, incr, length):
 def part1():
 
     length = 10007
-    top, deck_map, twentynineteen = Dll.init(length, specialval=2019)
+    top, _, twentynineteen = Dll.init(length, specialval=2019)
     for line in input:
         if "deal with increment" in line:
             incr = ints(line)[0]
@@ -153,152 +153,51 @@ def part1():
         i+=1
     aoc(i)
 
+DECK_LEN = 119315717514047
+TIMES = 101741582076661
 
-def spos(top, special):
-    i = 0
-    n = top
-    dir = top.dir()
-    def inc():
-        nonlocal n, dir
-        if dir == 1:
-            n = n.nxt()
-        else:
-            n = n.prv()
-    while n != special:
-        inc()
-        i+=1
-    return i
+# (with some help)
+# observe
+def cut_pos(cut, pos, deck_size):
+    return (pos - cut) % deck_size
+
+def deal_with_increment_pos(incr, pos, deck_size):
+    return (pos * incr) % deck_size
+
+def deal_new_stack(x, deck_size):
+    return deck_size - 1 - x
+
+# These are all of the form
+# a . pos + b mod deck_size
+# cut => a = 1, b = -cut
+# deal => a = incr, b = 0
+# stack => a = -1, b = (deck_size - 1)
+
+def forward(deck_size):
+    a, b = 1, 0 # identity function to start
+    for line in input:
+        if "deal with increment" in line:
+            incr = ints(line)[0]
+            a *= incr
+            b *= incr
+        if "deal into new stack" in line:
+            a *= -1
+            b = deck_size - 1 - b
+        if "cut" in line:
+            cutn = ints(line)[0]
+            b -= cutn
+    return a,b
 
 def part2():
+    a,b = forward(DECK_LEN)
+    # that's a function of shape (ax + b) mod N,
+    # its inverse is a^-1 mod p . x + -b . a^-1 mod p
+    c,d = pow(a, -1, DECK_LEN), -b * pow(a, -1, DECK_LEN)
+    # now repeated application is a geometric series
+    # 𝐹^𝑘(𝑥)=𝑎^𝑘𝑥+𝑏(1−𝑎^𝑘)/1−𝑎  mod 𝑚
+    e = pow(c, TIMES, DECK_LEN)
+    f = (e - 1) * pow(c-1, DECK_LEN - 2, DECK_LEN)
+    aoc(((e * 2020) + d*f) % DECK_LEN)
 
-    length = 10007
-    top, deck_map, twentytwenty = Dll.init(length, specialval=2020)
-
-    seen = set()
-    prev = [None] * 5
-    for i in range(975):
-        for line in input:
-            if "deal with increment" in line:
-                incr = ints(line)[0]
-                top = increment(top, incr, length)
-            if "deal into new stack" in line:
-                top = stack(top)
-            if "cut" in line:
-                cutn = ints(line)[0]
-                top = cut(top, cutn)
-
-        # p = spos(top, twentytwenty)
-        # prev.append(p)
-        # prev = prev[1:]
-        # if tuple(prev) in seen:
-        #     print("SEEN", i)
-        #     break
-        # seen.add(tuple(prev))
-    # SEEN 5007
-    # loops at 5002
-    #>>> 101741582076661 % 5002
-    #975
-        aoc(spos(top, twentytwenty))
-        # prev.append(p)
-        # prev = prev[1:]
-        # if tuple(prev) in seen:
-        #     print("SEEN", i)
-        #     break
-        # seen.add(tuple(prev))
-
-# def extended_euclidean(a, b):
-#     """
-#     Returns a pair of numbers (x, y) such that a * x + b * y = gcd(a, b)
-#     """
-#     if b == 0:
-#         return (1, 0)
-#     else:
-#         (x, y) = extended_euclidean(b, a % b)
-#         return (y, x - (a // b) * y)
-
-
-#part2()
-  
-def multiplicative_inverse(a, m):
-    """Find the multiplicative inverse of a modulo m."""
-    m0, x0, x1 = m, 0, 1
-    while a > 1:
-        # q is quotient
-        q = a // m
-        m, a = a % m, m
-        x0, x1 = x1 - q * x0, x0
-    return x1 + m0 if x1 < 0 else x1
-
-def solve_linear_congruence(a, b, L):
-    """Solve the linear congruence ax ≡ b mod L."""
-    d = gcd(a, L)
-    if b % d != 0:
-        return "No solution"
-
-    a, b, L = a // d, b // d, L // d
-    x = multiplicative_inverse(a, L) * b % L
-    return x
-
-DECK_LEN = 119315717514047
-
-def prev_pos_increment(current_pos, increment):
-    return solve_linear_congruence(increment, current_pos, DECK_LEN)
-
-def prev_pos_stack(current_pos):
-    return DECK_LEN - current_pos - 1
-
-def prev_pos_cut(current_pos, cut):
-    if cut > 0:
-        if current_pos >= DECK_LEN - cut:
-            return current_pos - (DECK_LEN - cut)
-        if current_pos < DECK_LEN - cut:
-            return current_pos + cut
-    if cut < 0:
-        if current_pos > (abs(cut) - 1):
-            return current_pos + cut # remember cut is -ve
-        else:
-            return DECK_LEN - (abs(cut) - current_pos)
-
-rules = []
-for line in input[-1:0:-1]:
-    if "deal with increment" in line:
-        incr = ints(line)[0]
-        rules.append((1,incr))
-    if "deal into new stack" in line:
-        rules.append((2,0))
-    if "cut" in line:
-        cutn = ints(line)[0]
-        rules.append((3,cutn))
-
-#history = [None] * 3
-states = set()
-def card_at_pos(pos, times=1):
-    global history, states, rules
-    seek = pos
-    for i in range(times):
-        for action,num in rules:
-                if action == 1:
-                    seek = prev_pos_increment(seek, num)
-                if action == 2:
-                    seek = prev_pos_stack(seek)
-                if action == 3:
-                    seek = prev_pos_cut(seek, num)
-        #print(i, seek)
-        #history.append(seek)
-        #history = history[1:]
-        if seek in states:
-            print("REPEAT at ", i)
-        states.add(seek)
-    return seek
-
-#unit tests with deck size == 10
-
-# assert(prev_pos_cut(8, 3) == 1)  # should be 1
-# assert(prev_pos_cut(7, 3) == 0)  # should be 0
-# assert(prev_pos_cut(6, 3) == 9)  # should be 9
-
-# assert(prev_pos_cut(4, -4) == 0) # should be 0
-# assert(prev_pos_cut(5, -4) == 1) # should be 1
-# assert(prev_pos_cut(3, -4) == 9) # should be 9
-
-print(card_at_pos(2020, 10000000))
+part1()
+part2()
