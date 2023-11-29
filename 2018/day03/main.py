3c891be8b24cb085089ca255f6eb3de3a636f1ff
David Singleton <davidsingleton@gmail.com>
2023-11-28 19:32:22 -0800

    cleaned up

--- 2018/day03/main.py
+++ 2018/day03/main.py
@@ -9,32 +9,27 @@ def part1():
     claims = defaultdict(int)
     
     for line in input:
-        p = ints(line.split("@")[1])
-        for x in range(p[0],p[0]+p[2]):
-            for y in range(p[1],p[1]+p[3]):
-                claims[(x,y)] += 1
+        id, x,y,w,h = ints(line)
+        for xx in range(x,x+w):
+            for yy in range(y,y+h):
+                claims[(xx,yy)] += 1
         
     aoc(sum([1 for _,v in claims.items() if v >= 2]))
 
 def part2():
     claims = defaultdict(int)
+    revclaims = defaultdict(list)
     
     for line in input:
-        p = ints(line.split("@")[1])
-        for x in range(p[0],p[0]+p[2]):
-            for y in range(p[1],p[1]+p[3]):
-                claims[(x,y)] += 1
+        id, x,y,w,h = ints(line)
+        for xx in range(x,x+w):
+            for yy in range(y,y+h):
+                claims[(xx,yy)] += 1
+                revclaims[id].append((xx,yy))
 
-    for line in input:
-        id = line.split("@")[0]
-        p = ints(line.split("@")[1])
-        found = True
-        for x in range(p[0],p[0]+p[2]):
-            for y in range(p[1],p[1]+p[3]):
-                if claims[(x,y)] > 1:
-                    found = False
-        if found:
-            aoc(id)
+    for k,v in revclaims.items():
+        if all([claims[p] == 1 for p in v]):
+            aoc(k)
             return
 
 part1()
