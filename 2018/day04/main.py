ef24d94231e2fbd8861b08d2c42c230111170b31
David Singleton <davidsingleton@gmail.com>
2023-11-28 20:40:15 -0800

    simplify (edge case never happens)

--- 2018/day04/main.py
+++ 2018/day04/main.py
@@ -9,32 +9,19 @@ input = [i.strip() for i in open("input","r").readlines()]
 
 def solve():
 
-    dates = [(l.split("]")[0][1:].replace('-','').replace(' ','').replace(':',''), l) for l in input]
-    ss = [r for _,r in sorted(dates)]
-
+    ss = sorted(input)
     guards = defaultdict(lambda : defaultdict(int))
-    guard = None
-    minute = 0
-    awake = True
     for line in ss:
         lmin = int(line.split("]")[0].split(":")[1])
 
         if 'begins shift' in line:
-            if awake == False:
-                for m in range(minute, 60):
-                    guards[guard][m] += 1
-
             guard = int(line.split("#")[1].split(" ")[0])
-            awake = True
-            minute = 0
         if 'falls asleep' in line:
             minute = lmin
-            awake = False
         if 'wakes' in line:
             for m in range(minute, lmin):
                 guards[guard][m] += 1
             minute = lmin
-            awake = True
 
     mmax,amax = 0,0
     mg,ag = None, None
@@ -42,11 +29,10 @@ def solve():
         asleep = sum([v for v in guards[guard].values()])
         sleepy_minute = max([v for v in guards[guard].values()])
         if sleepy_minute > mmax:
-            mmax = sleepy_minute
-            mg = guard
+            mmax, mg = sleepy_minute, guard
         if asleep > amax:
-            amax = asleep
-            ag = guard
+            amax,ag = asleep, guard
+
     # Part 1
     aoc(ag * sorted([(v,k) for k,v in guards[ag].items()])[-1][1])
     # Part 2
