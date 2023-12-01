53790d4271e83a51daa7da081e364be8153d3d43
David Singleton <davidsingleton@gmail.com>
2023-11-30 21:42:42 -0800

    use overlapped regex

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -1,6 +1,7 @@
 
 from utils import *
 import string
+import regex as re
 
 input = [i.strip() for i in open("input","r").readlines()]
 
@@ -8,7 +9,7 @@ def digs(s):
     return re.findall(r"\d", s)
 
 def nums(s):
-    return re.findall(r"(\d|one|two|three|four|five|six|seven|eight|nine)", s)
+    return re.findall(r"(\d|one|two|three|four|five|six|seven|eight|nine)", s, overlapped=True)
 
 def part1():
     tot = 0
@@ -18,24 +19,16 @@ def part1():
     aoc(tot)
 
 def part2():
-    tot = 0    
     # gah they can overlap!
     #Like this: oneight
     
     dig = {ch: i for i,ch in enumerate(string.digits)}
-    dig.update({"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9, "zero":0})
+    dig.update({"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9})
 
+    tot = 0
     for line in input:
-        for i in range(len(line)):
-            if nums(line[0:i]):
-                l = dig[nums(line[0:i])[0]]
-                break
-        for i in range(len(line)-1, -1, -1):
-            if nums(line[i:]):
-                r = dig[nums(line[i:])[0]]
-                break        
-        tot += int(str(l) + str(r))
-        
+        l,r = dig[nums(line)[0]], dig[nums(line)[-1]]
+        tot += int(str(l) + str(r))        
     aoc(tot)
 
 
