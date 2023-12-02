e8a4a1ffb81db558d87cc755dd9efb96a9618b6d
David Singleton <davidsingleton@gmail.com>
2023-12-02 18:06:23 +0000

    cleaner

--- 2023/day02/main.py
+++ 2023/day02/main.py
@@ -4,12 +4,12 @@ from utils import *
 input = [i.strip() for i in open("input","r").readlines()]
 
 def solve():
-    cap = {"red": 12, "green": 13, "blue": 14}
+    CAP = {"red": 12, "green": 13, "blue": 14}
     p1,p2 = 0, 0
     for game, line in enumerate(input, 1):
-            if all([not any(x>mm for x in map(int, re.findall(f"(\d+) {color}", line))) for color,mm in cap.items()]):
+            if all([not any(x>mm for x in map(int, re.findall(f"(\d+) {color}", line))) for color,mm in CAP.items()]):
                 p1 += game
-            p2 += reduce(operator.mul, [max(map(int, re.findall(f"(\d+) {color}", line))) for color in cap.keys()])
+            p2 += reduce(operator.mul, [max(map(int, re.findall(f"(\d+) {color}", line))) for color in CAP.keys()])
     print(p1, p2)
 
 solve()
