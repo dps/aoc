714b4b377a4a549ac6273c99d1e30a805d11395d
David Singleton <davidsingleton@gmail.com>
2023-12-03 21:55:30 -0800

    day4 memoized

--- 2023/day04/main.py
+++ 2023/day04/main.py
@@ -8,27 +8,24 @@ def solve():
     # Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
     cards = defaultdict(int)
     for num, line in enumerate(input, 1):
-        want = set(lmap(int, line.split("|")[0].split(":")[1].strip().split()))
-        have = set(lmap(int, line.split("|")[1].strip().split()))
+        line = line.split(":")[1].strip()
+        want = set(map(int, line.split("|")[0].strip().split()))
+        have = set(map(int, line.split("|")[1].strip().split()))
         cards[num] = len(want & have)
         if len(want & have) > 0:
             p1 += pow(2, len(want & have) - 1)
 
     print("part1", p1)
     
-    wins = defaultdict(int)
-
+    @cache
     def countup(num):
-        nonlocal cards, wins
         tt = cards[num]
-        for card_num in range(num + 1, num + tt + 1):
-            wins[card_num] += 1
-            countup(card_num)
-    
-    for num, line in enumerate(input, 1):
-        wins[num] += 1
-        countup(num)
+        return tt + sum((countup(n) for n in range(num + 1, num + tt + 1)))
 
-    print("part2", sum(wins.values()))
+    tot = 0    
+    for num, line in enumerate(input, 1):
+        tot += 1 + countup(num)
+
+    print("part2", tot)
 
 solve()
