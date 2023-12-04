525ce67bd9a9fc772dcb7e7ddf9ab3bd56412c44
David Singleton <davidsingleton@gmail.com>
2023-12-03 22:00:39 -0800

    cleanup

--- 2023/day04/main.py
+++ 2023/day04/main.py
@@ -1,31 +1,22 @@
 
 from utils import *
 
-input = [i.strip() for i in open("input","r").readlines()]
+p1,p2 = 0, 0
+# Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+scores = defaultdict(int)
+for num, line in enumerate(data(), 1):
+    line = line.split(":")[1].strip()
+    want = set(map(int, line.split("|")[0].strip().split()))
+    have = set(map(int, line.split("|")[1].strip().split()))
+    scores[num] = len(want & have)
+    if len(want & have) > 0:
+        p1 += pow(2, len(want & have) - 1)
 
-def solve():
-    p1 = 0
-    # Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-    cards = defaultdict(int)
-    for num, line in enumerate(input, 1):
-        line = line.split(":")[1].strip()
-        want = set(map(int, line.split("|")[0].strip().split()))
-        have = set(map(int, line.split("|")[1].strip().split()))
-        cards[num] = len(want & have)
-        if len(want & have) > 0:
-            p1 += pow(2, len(want & have) - 1)
+@cache
+def countup(num):
+    return scores[num] + sum((countup(n) for n in range(num + 1, num + scores[num] + 1)))
 
-    print("part1", p1)
-    
-    @cache
-    def countup(num):
-        tt = cards[num]
-        return tt + sum((countup(n) for n in range(num + 1, num + tt + 1)))
+for num, line in enumerate(data(), 1):
+    p2 += 1 + countup(num)
 
-    tot = 0    
-    for num, line in enumerate(input, 1):
-        tot += 1 + countup(num)
-
-    print("part2", tot)
-
-solve()
+print("part1", p1, "; part2", p2)
