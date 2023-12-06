1f2e2b9795ed54a2a8cecf552302bc475e5c89b7
David Singleton <davidsingleton@gmail.com>
2023-12-05 22:40:25 -0800

    add algebraic solution

--- 2023/day06/main.py
+++ 2023/day06/main.py
@@ -3,6 +3,12 @@ from utils import *
 input = [i.strip() for i in open("input", "r").readlines()]
 
 
+def quadratic(time, record):
+    x0 = int((time + math.sqrt(time*time - 4 * record))/2)
+    x1 = int((time - math.sqrt(time*time - 4 * record))/2)
+    a,b = min(x0,x1), max(x0,x1)
+    return b - a
+
 def solve():
     pt1, pt2 = 1, 0
 
@@ -17,9 +23,10 @@ def solve():
 
     time = ints(input[0].replace(" ", ""))[0]
     record = ints(input[1].replace(" ", ""))[0]
-    for i in range(1, time - 1):
-        if (time - i) * i > record:
-            pt2 += 1
+    pt2 = quadratic(time, record)
+    # for i in range(1, time - 1):
+    #     if (time - i) * i > record:
+    #         pt2 += 1
     print(pt1, pt2)
 
 
