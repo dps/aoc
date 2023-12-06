46ee3c596dc3338f17b328c2ecc2b64695abdde4
David Singleton <davidsingleton@gmail.com>
2023-12-05 21:24:46 -0800

    clean up

--- 2023/day06/main.py
+++ 2023/day06/main.py
@@ -1,26 +1,26 @@
-
 from utils import *
 
-input = [i.strip() for i in open("input","r").readlines()]
+input = [i.strip() for i in open("input", "r").readlines()]
+
 
 def solve():
-    pt1,pt2 = 1,0
+    pt1, pt2 = 1, 0
+
     times = ints(input[0])
     distances = ints(input[1])
-    td = list(zip(times, distances))
-
-    for time,record in td:
+    for time, record in zip(times, distances):
         c = 0
-        for i in range(1,time-1):
-            if (time-i)*i > record:
+        for i in range(1, time - 1):
+            if (time - i) * i > record:
                 c += 1
         pt1 *= c
 
-    time = ints(input[0].replace(" ",""))[0]
-    record = ints(input[1].replace(" ",""))[0]
-    for i in range(1,time-1):
-        if (time-i)*i > record:
+    time = ints(input[0].replace(" ", ""))[0]
+    record = ints(input[1].replace(" ", ""))[0]
+    for i in range(1, time - 1):
+        if (time - i) * i > record:
             pt2 += 1
-    print(pt1,pt2)
+    print(pt1, pt2)
+
 
 solve()
