b1c3aa32fe3293af8670b7be4a60553c1c23d41e
David Singleton <davidsingleton@gmail.com>
2023-12-05 22:53:52 -0800

    add the quadratic method for fun

--- 2023/day06/main.py
+++ 2023/day06/main.py
@@ -4,10 +4,13 @@ input = [i.strip() for i in open("input", "r").readlines()]
 
 
 def quadratic(time, record):
-    x0 = int((time + math.sqrt(time*time - 4 * record))/2)
-    x1 = int((time - math.sqrt(time*time - 4 * record))/2)
-    a,b = min(x0,x1), max(x0,x1)
-    return b - a
+    x1 = ((time - math.sqrt(time*time - 4 * record))/2)
+    x0 = ((time + math.sqrt(time*time - 4 * record))/2)
+    if int(x1) == x1:
+        x1 += 1
+    if int(x0) == x0:
+        x0 -= 1
+    return math.floor(x0) - math.ceil(x1) + 1
 
 def solve():
     pt1, pt2 = 1, 0
@@ -23,11 +26,15 @@ def solve():
 
     time = ints(input[0].replace(" ", ""))[0]
     record = ints(input[1].replace(" ", ""))[0]
-    pt2 = quadratic(time, record)
-    # for i in range(1, time - 1):
-    #     if (time - i) * i > record:
-    #         pt2 += 1
-    print(pt1, pt2)
+    for i in range(1, time - 1):
+        if (time - i) * i > record:
+            pt2 += 1
+    print("Brute force", pt1, pt2)
+
+    print("Quadratic method:")
+    print("pt1:", reduce(operator.mul, [quadratic(t,r) for (t,r) in zip(times, distances)]))
+    print("pt2:", quadratic(time, record))
+
 
 
 solve()
