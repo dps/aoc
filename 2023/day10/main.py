a1c0cd031027dd637b8a671b2541e1c8cdaa575b
David Singleton <davidsingleton@gmail.com>
2023-12-10 00:52:45 -0800

    2023 day 10

--- 2023/day10/main.py
+++ 2023/day10/main.py
@@ -3,62 +3,6 @@ from utils import *
 
 D = [i.strip() for i in open("input","r").readlines()]
 
-def old():
-    global D
-    tot = 0
-    
-    G,w,h = grid_from_strs(D)
-    start = None
-    for r in range(h):
-        for c in range(w):
-            if G[r][c] == 'S':
-                start = (c,r)
-
-    def grid_neighbors(p, width, height, dir):
-        for d in dir:
-            q = (p[0] + d[0], p[1] + d[1])
-            if q[0] < 0 or q[1] < 0 or q[0] >= width or q[1] >= height:
-                continue
-            yield (q)
-
-    NEIGHBORS = {
-        "S": [(0,-1),(0,1)], # by inspection of input data.
-        "-": [(1,0), (-1, 0)],
-        "L": [(0,-1), (1,0)],
-        "J": [(-1, 0), (0,-1)], 
-        "7": [(-1, 0), (0, 1)],
-        "F": [(0,1),(1,0)],
-        "|": [(0,-1),(0,1)],
-        ".": []
-    }
-    def neighbors(p):
-        ch = G[p[1]][p[0]]
-        dd = NEIGHBORS[ch]
-        for d in grid_neighbors(p, w, h, dd):
-            yield d
-
-    max_with_neighbor = (0, (start[0], start[1]))
-
-    initial_dirs = list(zip(list(neighbors(start)), [1] * 4, range(4)))
-    initial_set = set(zip(list(neighbors(start)),range(4)))
-    for i in zip([start] * 4, range(4)):
-        initial_set.add(i)
-
-    bfs, visited = deque(initial_dirs), initial_set
-
-    while bfs:
-        pos, depth, start_dir = bfs.popleft()
-        for d in neighbors(pos):
-            if d == start and depth > 2:
-                if depth % 2 == 1:
-                    return ((depth-1)//2)+1
-                else:
-                    return depth//2
-            if not (d, start_dir) in visited:
-                bfs.append((d, depth+1, start_dir))
-                visited.add((d, start_dir))
-        
-
 def part1():
     global D
     tot = 0
