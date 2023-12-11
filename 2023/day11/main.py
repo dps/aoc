bc53f8576e0aacc6f1d69502ba16b4d2682a90e1
David Singleton <davidsingleton@gmail.com>
2023-12-10 21:48:01 -0800

    2023 day11

--- 2023/day11/main.py
+++ 2023/day11/main.py
@@ -1,65 +1,33 @@
-
 from utils import *
 
-#input = [int(i.strip()) for i in open("input","r").readlines()]
-D = [i.strip() for i in open("input","r").readlines()]
+D = [i.strip() for i in open("input", "r").readlines()]
 
-def part1():
+
+def solve(part=1):
     global D
-    tot = 0
-    #max_sum = max([sum(map(int, lines)) for lines in bundles(D)])
-    
-    g,w,h,_ = grid_from_strs(D)
-    expand_rows, expand_cols = [], []
-    for y in range(h):
-        if all((c == '.' for c in g[y])):
-            expand_rows.append(y)
-    print(expand_rows)
-    for x in range(w):
-        if all((g[y][x] == '.' for y in range(h))):
-            expand_cols.append(x)
-    print(expand_cols)
-    expand_rows = sorted(expand_rows)
-    expand_cols = sorted(expand_cols)
 
-    def find_x(x):
-        exp = [xx for xx in expand_cols if xx < x]
-        return x + (1000000-1)*len(exp)
-    def find_y(y):
-        return y + (1000000-1)*len([yy for yy in expand_rows if yy < y])
-    
+    g, w, h, _ = grid_from_strs(D)
+    expand_rows = sorted([y for y in range(h) if all((c == "." for c in g[y]))])
+    expand_cols = sorted(
+        [x for x in range(w) if all((g[y][x] == "." for y in range(h)))]
+    )
+
+    def new_pos(x, y):
+        return (
+            x
+            + (1 if part == 1 else 999999) * len([xx for xx in expand_cols if xx < x]),
+            y
+            + (1 if part == 1 else 999999) * len([yy for yy in expand_rows if yy < y]),
+        )
+
     galaxies = set()
-    yy = 0
-    xx = 0
     for y in range(h):
         for x in range(w):
-            if g[y][x] == '#':
-                new_pos = (find_x(x), find_y(y))
-                galaxies.add(new_pos)
+            if g[y][x] == "#":
+                galaxies.add(new_pos(x, y))
 
-    print(galaxies)
+    aoc(sum((manhattan(a, b) for a, b in combinations(galaxies, 2))))
 
-    pairs = list(combinations(galaxies, 2))
-    print(len(pairs))
 
-    def gneighbors(p):
-        for d in DIR:
-            q = (p[0] + d[0], p[1] + d[1])
-            # if q[0] < 0 or q[1] < 0 or q[0] >= width or q[1] >= height:
-            #     continue
-            yield (1, q)
-
-    for a,b in pairs:
-        #print(".", a, b)
-        #dk = dynamic_a_star(gneighbors, a, b, manhattan)[0]
-        #print(".", a,b, dk)
-        #print(dk, manhattan(a,b))
-        dk = manhattan(a,b)
-        tot += dk
-        
-    
-        
-    aoc(tot)
-
-part1()
-#part2()
+solve(1)
+solve(2)
