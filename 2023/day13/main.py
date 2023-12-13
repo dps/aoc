32974d48ab38b3df3bb350a86b16fbc61b131796
David Singleton <davidsingleton@gmail.com>
2023-12-13 07:52:42 -0800

    cleanup day13

--- 2023/day13/main.py
+++ 2023/day13/main.py
@@ -1,14 +1,11 @@
 
 from utils import *
 
-#input = [int(i.strip()) for i in open("input","r").readlines()]
 D = [i.strip() for i in open("input","r").readlines()]
 
-def reflect(grid,w,h):
-    c, r = None, None
-    results = []
+def reflect(grid,w,h,target_diff=0):
     for col in range(1,w):
-        reflect = True
+        diff = 0
         for i in range(1, w):
             if col - i < 0 or col+(i-1) > w-1:
                 break
@@ -16,15 +13,12 @@ def reflect(grid,w,h):
                 a = grid[j][col-i]
                 b = grid[j][col+(i-1)]
                 if a != b:
-                    reflect = False
-                    break
-            if reflect == False:
-                break
-        if reflect:
-            results.append(col)
+                    diff += 1
+        if diff == target_diff:
+            return col
 
     for row in range(1,h):
-        reflect = True
+        diff = 0
         for i in range(1, h):
             if row - i < 0 or row+(i-1) > h-1:
                 break
@@ -32,57 +26,16 @@ def reflect(grid,w,h):
                 a = grid[row-i][j]
                 b = grid[row+(i-1)][j]
                 if a != b:
-                    reflect = False
-                    break
-            if reflect == False:
-                break
-        if reflect:
-            results.append(100 * row)
-    return results
+                    diff += 1
+        if diff == target_diff:
+            return (100 * row)
+
+
+p1,p2 = 0,0
+
+for grid in bundles(D):
+    g,w,h,_ = grid_from_strs(grid)
+    p1 += reflect(g,w,h,target_diff=0)
+    p2 += reflect(g,w,h,target_diff=1)
     
-
-
-def part1():
-    global D
-    tot = 0
-    #max_sum = max([sum(map(int, lines)) for lines in bundles(D)])
-    
-    for grid in bundles(D):
-        g,w,h,_ = grid_from_strs(grid)
-        tot += reflect(g,w,h)
-        
-    aoc(tot)
-
-def part2():
-    global D
-    tot = 0
-    
-    for grid in bundles(D):
-        g,w,h,_ = grid_from_strs(grid)
-
-        orig = set(reflect(g,w,h))
-        print(orig)
-
-        escape = False
-        for r in range(h):
-            for c in range(w):
-                print(r,c)
-                t = g[r][c]
-                print(t)
-                g[r][c] = "#" if t == "." else "."
-                print(g)
-                test = set(reflect(g,w,h))
-                test = test - orig
-                print("test", test)
-                if len(test) == 1:
-                    tot += test.pop()
-                    escape = True
-                    break
-                g[r][c] = t
-            if escape:
-                break
-        
-    aoc(tot)
-
-#part1()
-part2() # 29388 too low
+print("part1:", p1, " part2:", p2)
