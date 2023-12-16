e78474b9b8456b0364bfa240fb3177f928c1a7ba
David Singleton <davidsingleton@gmail.com>
2023-12-15 19:25:03 -0800

    simpler soln

--- 2023/day15/main.py
+++ 2023/day15/main.py
@@ -11,24 +11,21 @@ def hashfn(str):
 
 print("Part 1", sum(hashfn(v) for v in D[0].split(",")))
 
-boxes = defaultdict(list)
-lenses = defaultdict(int)
+boxes = defaultdict(lambda:{})
 for instr in D[0].split(","):
     box, label = hashfn(words(instr)[0]), words(instr)[0]
     op = ("remove", None) if instr[-1] == "-" else ("set", int(instr.split("=")[1]))
 
     if op[0] == "set":
-        if label in boxes[box]:
-            lenses[str(box) + "," + str(label)] = op[1]
-        else:
-            boxes[box].append(label)
-            lenses[str(box) + "," + str(label)] = op[1]
+        boxes[box][label] = op[1]
     elif op[0] == "remove":
-        if label in boxes[box]:
-            boxes[box].remove(label)
+        try:
+            del(boxes[box][label])
+        except:
+            pass
 
 tot = 0
 for box, ll in boxes.items():
     for slot,label in enumerate(ll, 1):
-        tot += (box+1) * slot * lenses[str(box) + "," + str(label)]
+        tot += (box+1) * slot * boxes[box][label]
 print("Part 2", tot)
