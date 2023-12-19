8235e473d0f32824d4078f0be155dd2c5dcf8cb5
David Singleton <davidsingleton@gmail.com>
2023-12-18 22:20:10 -0800

    2023 day19

--- 2023/day19/main.py
+++ 2023/day19/main.py
@@ -1,15 +1,10 @@
-
 from utils import *
 
-D = [i.strip() for i in open("input","r").readlines()]
-
+D = [i.strip() for i in open("input", "r").readlines()]
 tot = 0
-#max_sum = max([sum(map(int, lines)) for lines in bundles(D)])
-
 workflows, parts = bundles(D)
 
-
-wf={}
+wf = {}
 for f in workflows:
     name = f.split("{")[0]
     steps = f.split("{")[1].split("}")[0].split(",")
@@ -18,7 +13,7 @@ for f in workflows:
 # Part 1
 for p in parts:
     xmas = {}
-    for l,v in [x.split("=") for x in p[1:-1].split(",")]:
+    for l, v in [x.split("=") for x in p[1:-1].split(",")]:
         xmas[l] = int(v)
 
     done = False
@@ -49,14 +44,18 @@ print("Part 1", tot)
 # xmas
 def pos(ch):
     return "xmas".index(ch)
-R = ((1,4000),(1,4000),(1,4000),(1,4000))
+
+
+R = ((1, 4000), (1, 4000), (1, 4000), (1, 4000))
+
 
 def replace_range(pos, new_r, orig):
     return [orig[i] if i != pos else new_r for i in range(4)]
 
+
 def dfs(ranges, node):
     if node == "A":
-        return reduce(operator.mul, [(j-i)+1 for i,j in ranges])
+        return reduce(operator.mul, [(j - i) + 1 for i, j in ranges])
     if node == "R":
         return 0
     tot = 0
@@ -72,65 +71,14 @@ def dfs(ranges, node):
             val = int(val)
             if val >= rr[0] and val <= rr[1]:
                 if ss == ">":
-                    tot += dfs(replace_range(rp, (val+1,rr[1]), new_ranges), dest)
-                    new_ranges = replace_range(rp, (rr[0],val), new_ranges)
+                    tot += dfs(replace_range(rp, (val + 1, rr[1]), new_ranges), dest)
+                    new_ranges = replace_range(rp, (rr[0], val), new_ranges)
                 if ss == "<":
-                    tot += dfs(replace_range(rp, (rr[0],val-1), new_ranges), dest)
-                    new_ranges = replace_range(rp, (val,rr[1]), new_ranges)
+                    tot += dfs(replace_range(rp, (rr[0], val - 1), new_ranges), dest)
+                    new_ranges = replace_range(rp, (val, rr[1]), new_ranges)
         else:
             tot += dfs(new_ranges, rule)
     return tot
 
-print("Part 2", dfs(R,"in"))
 
-                    
-
-
-
-
-
-sys.exit(0)
-
-for p in parts:
-    print(p)
-    xmas = {}
-    for l,v in [x.split("=") for x in p[1:-1].split(",")]:
-        xmas[l] = int(v)
-    
-    done = False
-    state = "in"
-    states = set()
-    while not done:
-        if state in states:
-            print("loop", state)
-            continue
-        states.add(state)
-        flow = wf[state]
-        for step in flow:
-            if ":" in step:
-                opr = operator.lt if "<" in step else operator.gt #check
-                ss = "<" if "<" in step else ">"
-                print(opr, step.split(":")[0].split(ss)[0], xmas[step.split(":")[0].split(ss)[0]], int(step.split(":")[0].split(ss)[1]))
-                if opr(xmas[step.split(":")[0].split(ss)[0]], int(step.split(":")[0].split(ss)[1])):
-                    print("true")
-                    state = step.split(":")[1]
-                    if state == "A" or state == "R":
-                        done = True
-                    if state == "A":
-                        tot += sum(xmas.values())
-                    print("moving to", state)
-                    break
-                else:
-                    print("false")
-            else:
-                print("else moving to", step)
-                state = step
-                if state == "A" or state == "R":
-                    done = True
-                if state == "A":
-                    tot += sum(xmas.values())
-            
-
-
-    
-aoc(tot)
+print("Part 2", dfs(R, "in"))
