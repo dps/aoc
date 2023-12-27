301e27f09c037cff124bde65e573fc2b9cd66bc9
David Singleton <davidsingleton@gmail.com>
2023-12-27 12:52:04 -0800

    Faster karger's implementation

--- 2023/day25/kargers.py
+++ 2023/day25/kargers.py
@@ -12,28 +12,29 @@ for line in D:
     for e in to_.split(" "):
         vertices[e].add(from_)
 
-while True:
-    # Implementation based on https://en.wikipedia.org/wiki/Karger%27s_algorithm
-    # V maps node name to a list of nodes connected via edges (incl. repeats!) and a set
-    # of the original nodes which have been merged in to n.
-    V = {n: (list(v), set([n])) for n,v in vertices.items()}
+def kargers(vertices):
+    while True:
+        # Implementation based on https://en.wikipedia.org/wiki/Karger%27s_algorithm
+        # V maps node name to a list of nodes connected via edges (incl. repeats!) and a set
+        # of the original nodes which have been merged in to n.
+        V = {n: (list(v), set([n])) for n,v in vertices.items()}
 
-    while len(V.keys()) > 2:
-        e = random.choice(list(V.keys()))
-        f = random.choice(V[e][0])
-        
-        u,v = V[e], V[f]
+        while len(V.keys()) > 2:
+            e = random.choice(list(V.keys()))
+            f = random.choice(V[e][0])
 
-        for name,val in V.items():
-            if name == e:
-                V[name] = ([x for x in (u[0] + v[0]) if x != e and x != f], u[1]|v[1])
-            elif name == f:
-                continue
-            else:
-                V[name] = ([(e if d == f else d) for d in val[0]], val[1])
+            u,v = V[e], V[f]
 
-        del V[f]
+            for edge in v[0]:
+                if edge != e and edge != f:
+                    u[0].append(edge)
+                    V[edge][0].remove(f)
+                    V[edge][0].append(e)
+            V[e] = ([d for d in u[0] if d != f], u[1]|v[1])
 
-    if len(list(V.values())[0][0]) == 3:
-        aoc(reduce(operator.mul, [len(v[1]) for v in V.values()]))
-        sys.exit(0)
+            del V[f]
+
+        if len(list(V.values())[0][0]) == 3:
+            return reduce(operator.mul, [len(v[1]) for v in V.values()])
+
+print(kargers(vertices))
