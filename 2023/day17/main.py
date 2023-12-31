b74d2648e2b5f00a0f3b994f74a92e334256ea8b
David Singleton <davidsingleton@gmail.com>
2023-12-30 20:58:19 -0800

    37% speedup with a bucket heap

--- 2023/day17/main.py
+++ 2023/day17/main.py
@@ -5,16 +5,38 @@ D = [i.strip() for i in open("input", "r").readlines()]
 g, w, h, _ = grid_ints_from_strs(D)
 end = ((w - 1), (h - 1))
 
+class BucketHeap:
+    def __init__(self):
+        self.heap = defaultdict(list)
+        self.min_bucket = math.inf
+
+    def insert(self, element):
+        self.heap[element[0]].append(element)
+        if element[0] < self.min_bucket:
+            self.min_bucket = element[0]
+
+    def delete_min(self):
+        min_element = self.heap[self.min_bucket][0]
+        if len(self.heap[self.min_bucket]) > 1:
+            self.heap[self.min_bucket] = self.heap[self.min_bucket][1:]
+        else:
+            del self.heap[self.min_bucket]
+            try:
+                self.min_bucket = min(self.heap.keys())
+            except:
+                self.min_bucket = math.inf
+        return min_element
 
 def dynamic_dijkstra(neighbors, start, end):
-    q, seen, mins = [(0, start, [])], set(), {start: 0}
-    while q:
-        (cost, v, path) = heapq.heappop(q)
+    seen, mins = set(), {start: 0}
+    bh = BucketHeap()
+    bh.insert((0, start))
+    while True:
+        (cost, v) = bh.delete_min() #heapq.heappop(q)
         if v not in seen:
             seen.add(v)
-            path = path + [v]
             if v[0] == end:
-                return (cost, path)
+                return cost
 
             for c, neighbor in neighbors(v):
                 if neighbor in seen:
@@ -23,9 +45,8 @@ def dynamic_dijkstra(neighbors, start, end):
                 next = cost + c
                 if prev is None or next < prev:
                     mins[neighbor] = next
-                    heapq.heappush(q, (next, neighbor, path))
-
-    return math.inf, None
+                    bh.insert((next, neighbor))
+                    #heapq.heappush(q, (next, neighbor))
 
 
 def cw(d): return {(1, 0): (0, 1), (0, 1): (-1, 0), (-1, 0): (0, -1), (0, -1): (1, 0)}[d]
@@ -34,18 +55,35 @@ def ccw(d): return {(1, 0): (0, -1), (0, 1): (1, 0), (-1, 0): (0, 1), (0, -1): (
 def in_bounds(p, d): return 0 <= p[0] + d[0] < w and 0 <= p[1] + d[1] < h
 def cost(p, d): return g[p[1] + d[1]][p[0] + d[0]]
 
-def neighbors(state, part=1):
+def neighbors1(state):
+    r = []
     for p, d in [((0, 0), (1, 0)), ((0, 0), (0, 1))] if state == ("start") else [state]:
         cc = 0
-        for l in range(1, 4 if part == 1 else 11):
-            if in_bounds(p, (l * d[0], l * d[1])):
-                cc += cost(p, (l * d[0], l * d[1]))
-                if part == 1 or l >= 4:
-                    yield (cc, ((p[0] + l * d[0], p[1] + l * d[1]), cw(d)))
-                    yield (cc, ((p[0] + l * d[0], p[1] + l * d[1]), ccw(d)))
+        for l in range(1, 4):
+            dl = (l * d[0], l * d[1])
+            if in_bounds(p, dl):
+                cc += cost(p, dl)
+                r.append((cc, ((p[0] + l * d[0], p[1] + l * d[1]), {(1, 0): (0, 1), (0, 1): (-1, 0), (-1, 0): (0, -1), (0, -1): (1, 0)}[d])))
+                r.append((cc, ((p[0] + l * d[0], p[1] + l * d[1]), {(1, 0): (0, -1), (0, 1): (1, 0), (-1, 0): (0, 1), (0, -1): (-1, 0)}[d])))
             else:
                 break
+    return r
+
+def neighbors2(state):
+    r = []
+    for p, d in [((0, 0), (1, 0)), ((0, 0), (0, 1))] if state == ("start") else [state]:
+        cc = 0
+        for l in range(1, 11):
+            dl = (l * d[0], l * d[1])
+            if in_bounds(p, dl):
+                cc += cost(p, dl)
+                if l >= 4:
+                    r.append((cc, ((p[0] + l * d[0], p[1] + l * d[1]), {(1, 0): (0, 1), (0, 1): (-1, 0), (-1, 0): (0, -1), (0, -1): (1, 0)}[d])))
+                    r.append((cc, ((p[0] + l * d[0], p[1] + l * d[1]), {(1, 0): (0, -1), (0, 1): (1, 0), (-1, 0): (0, 1), (0, -1): (-1, 0)}[d])))
+            else:
+                break
+    return r
 
 # pypy3 main.py  1.16s user 0.02s system 99% cpu 1.187 total
-print("Part 1", dynamic_dijkstra(neighbors, ("start"), end)[0])
-print("Part 2", dynamic_dijkstra(lambda s: neighbors(s, part=2), ("start"), end)[0])
+print(dynamic_dijkstra(neighbors1, ("start"), end))
+print(dynamic_dijkstra(neighbors2, ("start"), end))
