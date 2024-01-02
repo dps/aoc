75a2160d2e8ad7e195d999ee537dec7a1c4efacb
David Singleton <davidsingleton@gmail.com>
2024-01-01 19:57:51 -0800

    switch to a non-regex soln for 3x speedup

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -1,34 +1,21 @@
-
-import string
-import regex as re
-
 input = [i.strip() for i in open("input", "r").readlines()]
 
-def digs(s):
-    return re.findall(r"\d", s)
+def solve():
+    dig = {"one":"1","two":"2","three":"3","four":"4","five":"5","six":"6","seven":"7","eight":"8","nine":"9"}
 
-def nums(s):
-    return re.findall(r"(\d|one|two|three|four|five|six|seven|eight|nine)", s, overlapped=True)
-
-def part1():
-    tot = 0
+    p1,p2 = 0,0
     for line in input:
-        l,r = digs(line)[0], digs(line)[-1]
-        tot += int(l + r)        
-    print(tot)
+        ns1,ns2 = [],[]
+        for i, ch in enumerate(line):
+            if ch.isdigit():
+                ns1.append(ch)
+                ns2.append(ch)
+            for dd,vv in dig.items():
+                if line[i:].startswith(dd):
+                    ns2.append(vv)
+        p1 += int(ns1[0] + ns1[-1])
+        p2 += int(ns2[0] + ns2[-1])
+                
+    print(p1, p2)
 
-def part2():
-    # gah they can overlap!
-    #Like this: oneight
-    
-    dig = {ch: i for i,ch in enumerate(string.digits)}
-    dig.update({"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9})
-
-    tot = 0
-    for line in input:
-        l,r = dig[nums(line)[0]], dig[nums(line)[-1]]
-        tot += int(str(l) + str(r))        
-    print(tot)
-
-part1()
-part2()
+solve()
