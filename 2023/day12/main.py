174e65e4620640275b6d79a4d89b81149cc72cc8
David Singleton <davidsingleton@gmail.com>
2024-01-06 22:30:19 -0800

    minor speedup

--- 2023/day12/main.py
+++ 2023/day12/main.py
@@ -17,10 +17,13 @@ def ddfs(pattern, counts):
 
     # no more patterns to make, rest has to be "." or "?"=>"."
     if len(counts) == 0:
-        if all([p != "#" for p in pattern]):
-            return 1
-        else:
-            return 0
+        for p in pattern:
+            if p == '#': return 0
+        return 1
+        # if all([p != "#" for p in pattern]):
+        #     return 1
+        # else:
+        #     return 0
 
     if pattern[0] == ".":
         return ddfs(pattern[1:], counts)
@@ -29,7 +32,7 @@ def ddfs(pattern, counts):
     if pattern[0] == "?": # consider the . case
         tot += ddfs(pattern[1:], counts)
     
-    # make a whole group here if we can
+    # replacing this all is no appreciable speedup
     if all([c != "." for c in pattern[0:counts[0]]]) and ((pattern[counts[0]] != "#") if len(pattern) > counts[0] else True):
         tot += ddfs(pattern[counts[0] + 1:], counts[1:])
 
