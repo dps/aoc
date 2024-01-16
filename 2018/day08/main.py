6cce743fe51bcdd5fc7024c1d8605ddff3521d75
David Singleton <davidsingleton@gmail.com>
2024-01-15 21:55:42 -0800

    little clean up

--- 2018/day08/main.py
+++ 2018/day08/main.py
@@ -1,27 +1,23 @@
 
 gen = (int(i) for i in open("input","r").readlines()[0].split())
+metadata = 0
 
-metadata = []
 def node_value():
     global gen, metadata
     children, mds = next(gen), next(gen)
-    cvals = []
+    cvals, val = [], 0
     for _ in range(children):
         cvals.append(node_value())
     assert(len(cvals) == children)
-    val = 0
     for _ in range(mds):
         m = next(gen)
-        metadata.append(m)
+        metadata += m
         if children == 0:
             val += m
-        else:
-            if m == 0:
-                continue
-            elif len(cvals) >= m:
-                val += cvals[m-1]
+        elif m > 0 and len(cvals) >= m:
+            val += cvals[m-1]
     return val
 
 root = node_value()
-print("day08", sum(metadata), root)
+print("day08", metadata, root)
 
