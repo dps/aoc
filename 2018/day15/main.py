48517e02d16adcde9feddcc4dea5d60bb49e9835
David Singleton <davidsingleton@gmail.com>
2024-01-20 14:52:09 -0800

    2018 day 15

---
+++ 2018/day15/main.py
@@ -0,0 +1,118 @@
+
+from utils import *
+
+D = [i.strip() for i in open("input","r").readlines()]
+G = list("".join(D))
+N = len(G)
+w,h = len(D[0]), len(D)
+
+U = {}
+
+def points(found):
+    global U
+    u = U[found[0]+found[1]]
+    return u[2]*10000+u[0]
+
+def opponent(ch):
+    return ('E' if ch == 'G' else 'G')
+
+def hp_then_p(unit):
+    p,_,hp,_,_ = unit
+    return (hp,p)
+
+def is_combat(unit):
+    global G
+    p,ch,hp,power,name = unit
+    enemy = opponent(ch)
+    could_fight = []
+    for d in [-w,-1,1,w]:
+        if 0 <= p+d < N and G[p+d] == enemy:
+            could_fight.append(U[p+d])
+    if len(could_fight) == 0:
+        return False, None
+    else:
+        could_fight.sort(key=hp_then_p)
+        return True, could_fight[0][0]
+
+def move(unit):
+    global G
+    p,ch,hp,power,name = unit
+    enemy = opponent(ch)
+    Q, visited, found_at, found = deque([(p,0,None)]),set([p]),math.inf,[]
+    while Q:
+        q,l,first_step = Q.popleft()
+        if l > found_at:
+            continue
+        for d in [-w,-1,1,w]:
+            fs = d if first_step == None else first_step
+            p_ = q + d
+            if p_ not in visited and 0 <= p_ < N:
+                if G[p_] == enemy and l <= found_at:
+                    found_at = l
+                    found.append((p_,fs))
+                elif G[p_] == '.':
+                    visited.add(p_)
+                    Q.append((p_,l+1,fs))
+    if len(found) == 0:
+        return None
+    found.sort()
+    return found[0][1]
+
+
+def battle(epower, stop_on_elf_death=False):
+    global G, U
+    G = list("".join(D))
+    U = {}
+    units = []
+    for i,ch in enumerate(G):
+        if ch == 'E' or ch == 'G':
+            U[i] = (i,ch,200,3 if ch == 'G' else epower,i)
+            units.append(U[i])
+
+    rounds,p = 0,0
+    while True:
+        units.sort()
+                
+        to_delete = set()
+        for u in units:
+            if u[4] in to_delete:
+                continue
+
+            p,ch,hp,power,name = U[u[0]]
+            
+            combat, where = is_combat(u)
+            if not combat:
+                dir = move(u)
+                if dir != None:
+                    G[p],G[p+dir] = '.', ch
+                    del(U[p])
+                    U[p+dir] = (p+dir,ch,hp,power,name)
+                    combat, where = is_combat(U[p+dir])
+
+            if combat:
+                p_,ch_,hp_,power_,name_ = U[where]
+                #assert(ch_ == opponent(ch))
+                #assert(p_ == where)
+                U[where] = (p_,ch_,hp_-power,power_,name_)
+                if hp_-power <= 0:
+                    if stop_on_elf_death and ch_ == 'E':
+                        return False, None
+                    to_delete.add(name_)
+                    G[where] = '.'
+                    del(U[where])
+
+        units = [u for u in U.values() if u[4] not in to_delete]
+        if len(Counter([u[1] for u in units]).keys()) > 1:
+            rounds += 1
+        else:
+            return True, rounds * sum([u[2] for u in units])
+
+
+
+print("Part 1", battle(3, stop_on_elf_death=False)[1])
+
+for elf_power in range(4,20):
+    success, val = battle(elf_power, stop_on_elf_death=True)
+    if success:
+        print("Part 2", val)
+        break
