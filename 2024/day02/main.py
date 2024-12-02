57d587e06edbbd003583735327823229aa90042e
David Singleton <davidsingleton@gmail.com>
2024-12-01 23:05:19 -0800

    2024 day2

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -1,33 +1,20 @@
 
 from utils import *
 
-D = [l.strip() for l in open("input").readlines()]
-
-p1,p2 = 0,0
+D = [ints(l) for l in open("input").readlines()]
 
 def should_count(ii):
     dir = None
-    skip = False
     for p,n in zip(ii, ii[1:]):
         if dir == None:
             dir = sign(n - p)
-        else:
-            if sign(n - p) != dir:
-                return False
-        
+        elif sign(n - p) != dir:
+            return False
         if abs(n - p) > 3 or n == p:
             return False
     return True
 
-for line in D:
-    ii = ints(line)
-    if should_count(ii):
-        p1 += 1
-    test = combinations(ii, len(ii)-1)
-    for t in test:
-        if should_count(t):
-            p2 += 1
-            break
-
+p1 = sum([should_count(line) for line in D])
+p2 = sum([any(should_count(t) for t in combinations(line, len(line) - 1)) for line in D])
 
 print(p1, p2)
