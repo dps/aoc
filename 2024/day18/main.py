459e99afab9854afd5285941739d584a286f562b
David Singleton <davidsingleton@gmail.com>
2024-12-17 23:08:13 -0800

    little cleaner

--- 2024/day18/main.py
+++ 2024/day18/main.py
@@ -4,6 +4,8 @@ from utils import *
 D = [i.strip() for i in open("input","r").readlines()]
 
 p1, p2 = 0, 0
+start = (0,0)
+end = (70,70)
 
 P = D[0:1024]
 grid = defaultdict(int)
@@ -17,14 +19,14 @@ def neighbors(p):
         if x+dx >= 0 and y+dy >= 0 and x+dx <= 70 and y+dy <= 70 and grid[(x+dx,y+dy)] == 0:
             yield (1, (x+dx,y+dy))
     
-p1, path = dynamic_dijkstra(neighbors, (0,0), (70,70))
+p1, path = dynamic_dijkstra(neighbors, start, end)
 
 for d in D[1024:]:
     x,y = ints(d)
     grid[(x,y)] = 1
     if (x,y) not in path:
         continue
-    a, path = dynamic_dijkstra(neighbors, (0,0), (70,70))
+    a, path = dynamic_dijkstra(neighbors, start, end)
     if a == math.inf:
         p2 = (x,y)
         break
